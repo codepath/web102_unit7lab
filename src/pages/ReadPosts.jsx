24689f95734020f1d2f6d4c497b91be44a4403f8
kyra-ptn <kyra@codepath.org>
2025-07-09 13:40:44 -0400

    Add key to Card

--- src/pages/ReadPosts.jsx
+++ src/pages/ReadPosts.jsx
@@ -14,7 +14,13 @@ const ReadPosts = (props) => {
             {
                 posts && posts.length > 0 ?
                 posts.map((post,index) => 
-                    <Card id={post.id} title={post.title} author={post.author} description={post.description}/>
+                    <Card 
+                        key={post.id}
+                        id={post.id} 
+                        title={post.title}
+                        author={post.author}
+                        description={post.description}
+                    />
                 ) : <h2>{'No Challenges Yet 😞'}</h2>
             }
         </div>  
